91a210b1a24eb451e70a64b528334cadfffdbd79
ER-Mollo <mollorefilwe68@gmail.com>
2022-06-20 15:26:29 +0200

    Added my project

--- src/components/signup.js
+++ src/components/signup.js
@@ -1,22 +1,30 @@
 
-
+import React, {useState} from 'react'
 import {useHistory} from 'react-router-dom'
 import {Link} from 'react-router-dom'
+import {createUserWithEmailAndPassword} from 'firebase/auth';
+import {auth} from '../config/firebase'
 function Signup(){
-    const btn = {
-        width: '150px'
-    }
+    
+    const [email, setEmail]= useState('');
+    const [password, setPassword]= useState("");
     let history = useHistory();
 
-    // const login = (()=>{
-    //     history.push("/home");
-    // })
+    const Register = ()=>{
+
+        createUserWithEmailAndPassword(auth, email, password).then(()=>{
+            history.push("/home");
+        }).catch((error)=>{
+            console.log(error)
+        })
+        
+    }
     return (
         <div className="container">
             <h1>Sign Up</h1>
-            <input type="email" placeholder="Enter your mail"/><br></br>
-            <input type="password" placeholder="Enter your password"/>
-            <button style={btn}>Sign up</button>
+            <input type="email" placeholder="Enter your mail" onChange={(e) => setEmail(e.target.value)}/><br></br>
+            <input type="password" placeholder="Enter your password" onChange={(e) => setPassword(e.target.value)}/>
+            <button onClick={Register}>Sign up</button>
             <span>Already have an account</span>
             <span>
                 <Link to='/'>login here</Link>
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,11 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import {getAuth} from 'firebase/auth'
 const firebaseConfig = {
   apiKey: "AIzaSyCXFWrwNCx2cB5kVcaGQlO2cegs6o0aLCE",
   authDomain: "budget-app-d0dd6.firebaseapp.com",
@@ -18,4 +14,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
 const analytics = getAnalytics(app);
+export  {auth}
